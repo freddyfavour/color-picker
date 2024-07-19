81d44b6946fec7d658c916471cc48f74173b57c4
freddyfavour <alfredfavour76@gmail.com>
2024-07-19 19:19:42 +0100

    Added Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"], // include all possible file extensions
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};

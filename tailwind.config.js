1661c459f2d35ea58c3dbbdc71eeabbb835350cd
freddyfavour <alfredfavour76@gmail.com>
2024-07-19 19:24:49 +0100

    deleted all files

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"], // include all possible file extensions
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};

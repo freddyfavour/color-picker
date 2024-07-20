b6ced52c0d620bf17a6330e7f50a55af792e80cf
freddyfavour <alfredfavour76@gmail.com>
2024-07-20 12:22:02 +0100

    tailwind and font setup

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="bg-slate-300 h-screen flex items-center justify-center font-sans">
+      <p className="text-6xl text-gray-900 text-center">Testing App</p>
+    </div>
+  );
+}
